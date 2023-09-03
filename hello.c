cfdd4666d09144dde142f8480c539a4607c18418
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-03 13:44:03 +0900

    四則演算

--- hello.c
+++ hello.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(void){
-    printf("%d¥", 100+200);
+    printf("%d", 100+200);
+    printf("%d", 10 + 3);
+    printf("%d",10 - 3);
+    printf("%d",10 * 3);
+    printf("%d",10 / 3);
+    printf("%d",10 % 3);
     return 0;
 }
+
